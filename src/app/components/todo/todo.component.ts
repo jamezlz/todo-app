57f5377974a045a40b8d819fe4a4f4be97cfccfa
jamezlz <35937551+jamezlz@users.noreply.github.com>
2021-03-24 16:17:37 -0500

    Basic app completed with Adding and deleting todos

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  todos: any[];
+  newTodo = "";
+
+  constructor(private service: TodoService) { }
 
   ngOnInit(): void {
+    this.todos = [];
+    this.service.getTodos().subscribe(data=>{
+      this.todos = data;
+    });
+  }
+
+  addTodo() {
+    if (this.newTodo) {
+      let date = new Date();
+      this.service.addTodo({data: this.newTodo, date: date.toISOString()}).subscribe(data=>{
+        this.todos.push(data);
+        this.newTodo = "";
+      });
+    }
+  }
+
+  finish(todo) {
+    this.service.deleteTodo(todo.id).subscribe(data=>{
+      let index=0;
+      for (let i = 0; i<this.todos.length;i++) {
+        if (this.todos[i].id === todo.id) {
+          index = i;
+        }
+      }
+      this.todos.splice(index,1);
+    });
   }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  url = "http://localhost:8080/todos/";
+
+  constructor(private http: HttpClient) { }
+
+  getTodos(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  getTodo(id): Observable<any> {
+    return this.http.get(this.url+id);
+  }
+
+  deleteTodo(id): Observable<any> {
+    return this.http.delete(this.url+id);
+  }
+
+  updateTodo(todo): Observable<any> {
+    return this.http.put(this.url+todo.id,todo);
+  }
+
+  addTodo(todo): Observable<any> {
+    return this.http.post(this.url,todo);
+  }
+
 }
